193b36f77b060ea9e5fa14436216cb4cdaa3403a
Azharhashmi111 <88384327+Azharhashmi111@users.noreply.github.com>
2025-07-08 22:07:57 +0530

    Update BankingApplication.java

--- src/main/java/com/project/staragile/banking/BankingApplication.java
+++ src/main/java/com/project/staragile/banking/BankingApplication.java
@@ -1,7 +1,10 @@
 package com.project.staragile.banking;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.MeterRegistryCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankingApplication {
@@ -10,4 +13,9 @@ public class BankingApplication {
         SpringApplication.run(BankingApplication.class, args);
     }
 
+    // 👇 Bean to ensure Micrometer is configured with tags (needed for Prometheus)
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "banking-service");
+    }
 }
