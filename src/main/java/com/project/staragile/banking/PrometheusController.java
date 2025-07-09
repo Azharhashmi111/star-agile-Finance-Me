c492b15e843e20785e44f00edc35d0dd17da126a
Azharhashmi111 <88384327+Azharhashmi111@users.noreply.github.com>
2025-07-09 10:19:43 +0530

    Create PrometheusController.java

---
+++ src/main/java/com/project/staragile/banking/PrometheusController.java
@@ -0,0 +1,44 @@
+package com.project.staragile.banking;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micrometer.prometheus.PrometheusConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class BankingApplication {
+
+    private final MeterRegistry meterRegistry;
+
+    public BankingApplication(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingApplication.class, args);
+    }
+
+    @Bean
+    public PrometheusMeterRegistry prometheusMeterRegistry() {
+        return new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "finance-world");
+    }
+
+    @PostConstruct
+    public void initCustomMetric() {
+        if (meterRegistry instanceof PrometheusMeterRegistry) {
+            PrometheusMeterRegistry promRegistry = (PrometheusMeterRegistry) meterRegistry;
+            promRegistry.counter("custom_metric_initialized_total", "purpose", "init").increment();
+        }
+    }
+}
